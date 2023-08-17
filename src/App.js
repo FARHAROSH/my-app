c5e19bcf8d9088defb5cb784767717292b87021f
FARHAROSH <farhatmubeena121@gmail.com>
2023-08-18 01:24:10 +0400

    APP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
 import './App.css';
+import Home from './home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
+    <div className="">
+      <header >
+       <Home/>
       </header>
     </div>
   );
